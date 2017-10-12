cef47cd9fb437081b0d634be26cc94b63f168b9b
king0120 <king0120@gmail.com>
2017-10-12 16:40:50 -0400

    consuming api data and showing on idea page

--- client/src/components/idea/IdeaPage.js
+++ client/src/components/idea/IdeaPage.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
 
 class IdeaPage extends Component {
+  state={
+    user: {
+      userName: '',
+      password: '',
+      ideas: []
+    }
+  }
+
+  async componentWillMount () {
+    const { userId } = this.props.match.params
+    const res = await axios.get(`/api/users/${userId}`)
+    this.setState({user: res.data})
+  }
+
   render () {
     return (
       <div>
-        <h1>Hello From IdeaPage</h1>
+        <h1>{this.state.user.userName}'s Idea Board</h1>
+        {this.state.user.ideas.map((idea) => {
+          return (
+            <div key={idea._id}>
+              <h3>{idea.title}</h3>
+              <p>{idea.description}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
--- routes/UsersController.js
+++ routes/UsersController.js
@@ -11,6 +11,15 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    res.json(user)
+  } catch (err) {
+    res.send(err)
+  }
+})
+
 router.post('/', async (req, res) => {
   try {
     const newUser = new User(req.body.user)
