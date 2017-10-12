d22a1917a5844ce45c6c57e6d563c53b8dd51bc0
king0120 <king0120@gmail.com>
2017-10-12 16:21:33 -0400

    added sign-up post

--- client/src/components/login/SignUpForm.js
+++ client/src/components/login/SignUpForm.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class SignUpForm extends Component {
+  state = {
+    newUser: {
+      userName: '',
+      password: ''
+    }
+  }
+
+  handleChange = (event) => {
+    const attribute = event.target.name
+    const updateUser = {...this.state.newUser}
+    updateUser[attribute] = event.target.value
+    this.setState({newUser: updateUser})
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault()
+    const res = await axios.post('/api/users', {
+      'user': this.state.newUser
+    })
+    console.log(res.data)
+  }
+
   render () {
     return (
       <div>
         <h1>Sign-Up</h1>
-        <form >
+        <form onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="userName">User Name</label>
-            <input  name="userName" type="text" />
+            <input
+              onChange={this.handleChange} name="userName"
+              type="text" value={this.state.newUser.userName}
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
-            <input  name="password" type="text" />
+            <input onChange={this.handleChange}
+              value={this.state.newUser.password}
+              name="password" type="text" />
           </div>
           <button>Sign Up</button>
         </form>
